e632a6fd3642d61674914975c3daf77849cbdd20
Angela Damaso <ajd092@gmail.com>
2018-09-26 21:34:35 -0400

    adds initial styling

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import styles from './styles.js'
+import Home from './screens/Home.js'
+import { 
+  Text, 
+  View 
+} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Home/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
